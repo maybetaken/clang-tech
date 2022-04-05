c5515c25a4554ba6fa005266442a5e8f39dc87e4
maybetaken <jiangyabin8@gmail.com>
2022-04-05 17:28:44 +0800

    session 3-3

--- 3/test.c
+++ 3/test.c
@@ -2,31 +2,28 @@
  
 int main()
 {
-   int a = 21;
-   int b = 10;
-   int c ;
-#if 0
-   /*
-   c = a + b;
-   printf("Line 1 - c 的值是 %d\n", c );
-   c = a - b;
-   printf("Line 2 - c 的值是 %d\n", c );
-   c = a * b;
-   printf("Line 3 - c 的值是 %d\n", c );
-   c = a / b;
-   printf("Line 4 - c 的值是 %d\n", c );
-   c = a % b;
-   printf("Line 5 - c 的值是 %d\n", c );
-      */
-#endif
-   c = a++;  // 赋值后再加 1 ，c 为 21，a 为 22
-   printf("Line 6 - c 的值是 %d\n", c );
-   c = a--;  // 赋值后再减 1 ，c 为 22 ，a 为 21
-   printf("Line 7 - c 的值是 %d\n", c );
-
-
-   c = ++a;  //先自加1， 然后再赋值。 a 22 c 22
-   printf("value of c is %d, value of a is %d \n",c , a);
-
-   c = --a; // 先自减1， 然后再赋值， a 21 , c 21
+   int a = 4;
+   short b;
+   double c;
+   int* ptr;
+   short* pb;
+   double* pc;
+ 
+   /* sizeof 运算符实例 */
+   printf("Line 1 - 变量 a 的大小 = %lu\n", sizeof(a) );
+   printf("Line 2 - 变量 b 的大小 = %lu\n", sizeof(b) );
+   printf("Line 3 - 变量 c 的大小 = %lu\n", sizeof(c) );
+ 
+   /* & 和 * 运算符实例 */
+   ptr = &a;    /* 'ptr' 现在包含 'a' 的地址 */
+   printf("a 的值是 %d\n", a);
+   printf("*ptr 是 %d\n", *ptr);
+ 
+   /* 三元/目运算符实例 */
+   a = 10;
+   b = (a == 1) ? 20: 30;
+   printf( "b 的值是 %d\n", b );
+ 
+   b = (a == 10) ? 20: 30;
+   printf( "b 的值是 %d\n", b );
 }
